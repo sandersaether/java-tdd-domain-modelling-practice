9d71bf611d3ba574e7c8e0354500d758ee2b54b3
Sandersaether <sander.sather@gmail.com>
2024-01-16 10:15:41 +0100

    test for user story 1

---
+++ src/main/java/com/booleanuk/RandomStudent.java
@@ -0,0 +1,8 @@
+package com.booleanuk;
+
+public class RandomStudent {
+
+    public String showMenu(){
+        return "";
+    }
+}
--- src/test/java/com/booleanuk/core/CoreTest.java
+++ src/test/java/com/booleanuk/core/CoreTest.java
@@ -1,4 +1,23 @@
 package com.booleanuk.core;
 
+import com.booleanuk.RandomStudent;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class CoreTest {
+
+    @Test
+    public void testMenu(){
+        RandomStudent random = new RandomStudent();
+        String expected = """
+                Choose option:
+                1. Pick random student
+                2. Pick random pairs
+                3. Pick random groups
+                4. Exit
+                """;
+        Assertions.assertEquals(expected, random.showMenu());
+
+    }
+
 }
