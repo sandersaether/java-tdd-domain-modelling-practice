6016e8deb605fa840af1988a0087c4c64ea36dc6
Sandersaether <sander.sather@gmail.com>
2024-01-16 10:17:53 +0100

    code to pass first test

--- src/main/java/com/booleanuk/RandomStudent.java
+++ src/main/java/com/booleanuk/RandomStudent.java
@@ -3,6 +3,12 @@ package com.booleanuk;
 public class RandomStudent {
 
     public String showMenu(){
-        return "";
+        return """
+                Choose option:
+                1. Pick random student
+                2. Pick random pairs
+                3. Pick random groups
+                4. Exit
+                """;
     }
 }
